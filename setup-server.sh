73b4aa5439205ea4340cbe3f480d03cd837caf0c
NanoCode012 <kevinvong@rocketmail.com>
2021-04-14 17:55:13 +0700

    Pass in temp string as env

--- setup-server.sh
+++ setup-server.sh
@@ -121,7 +121,7 @@ read_yes_no check_create_new_backend_app "Create new node app"
 if [ "$check_create_new_backend_app" = true ]; then
     echo "Creating new node app"
     temp = "cd backend && npm init -y && npm install --save-dev nodemon && npm set-script start \"node src/index.js\" && npm set-script test \"nodemon src/index.js\""
-    sudo docker run --rm -v $(pwd)/backend:/backend node:$DOCKER_NODE_VERSION /bin/sh -c $temp
+    sudo docker run --rm -v $(pwd)/backend:/backend -e $temp node:$DOCKER_NODE_VERSION /bin/sh -c $temp
     sudo chown -R ${USER}:${USER} backend
     echo "Created new node app"
 fi
